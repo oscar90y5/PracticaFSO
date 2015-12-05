11c350fc9e0fde16c9d7abecd9fdb8e11a913288
oscar90y5 <oscarfernandezangulo@gmail.com>
2015-12-05 21:30:06 +0100

    funciona, mas o menos, sin concurrencia

--- practica.c
+++ practica.c
@@ -31,87 +31,106 @@ void *productor(void *arg){
 	char *nombreFich = argumentos[2];
 	int tam = atoi(argumentos[1]);	
 	char palabra[10];
-	
+	int tmp=0;
+	char indice=0;
 	
 	printf("%d\n", tam);
 	int contador = 0;
 	archivo = fopen(nombreFich,"r");
-	while(fgets(palabra, 10, archivo)!= NULL){
-		printf("prod1\n"); fflush(stdout);
-		sem_wait(&espacioB1);
-		printf("prod2\n"); fflush(stdout);
-		sprintf(buffer1[contador],"%s", palabra);
-		sem_post(&datoB1);
-		if(contador<tam-1){
-			contador++;
+	do{
+		tmp =fgetc (archivo);
+	//while(tmp= fgetc(archivo)!=EOF){
+		if (tmp == '\n'||tmp==EOF){
+			indice=0;
+			if(contador<tam-1){
+				contador++;
+			}else{
+				contador=0;
+			}
 		}else{
-			contador=0;
-		}
+			buffer1[contador][indice]=tmp;
+			indice++;
+		}	
+		printf("t%c",tmp);
+//	}
+	}while (tmp != EOF);
 		
-	}
-	pthread_exit(0);
+//		sem_wait(&espacioB1);
+	
+//		sem_post(&datoB1);
+		
+	
+	
+//	pthread_exit(0);
 }
 
 void *consumidor1 (void *arg){
 	char ** argumentos = (char**) arg;
 	int tam = atoi(argumentos[1]);
-	int i,j=0;
-	char palabra[8];
+	int i=0,j=0,tmp=0;
+	char palabra[20];
 	//Coge palabra, cuantas veces?
-	while(1>0){
-		//falta tami
-		for(i=0;i<tam;i++,j++){
-			printf("antescon\n");
-			sem_wait(&datoB1);
-			printf("despuescon\n");
-			if(palindromo(buffer1[i])){
-				sprintf(palabra,"pal si");	
-			}else{
-				sprintf(palabra,"pal no");
-			}
-			sem_post(&espacioB1);
-			if(j<5){
-				sprintf(buffer2[j],"%s",palabra);
-				
-			}else{
-				j=0;
-				sprintf(buffer2[j],"%s",palabra);
-			}				
+	while(strcmp(buffer1[i],"eof")!=0){
+		printf("3\n");
+//		sem_wait(&datoB1);
+		if(palindromo(buffer1[i])){
+			sprintf(palabra,"%s si", buffer1[i]);	
+		}else{
+			sprintf(palabra,"%s no", buffer1[i]);
 		}
+//		sem_post(&espacioB1);
+		if(j<5){
+			sprintf(buffer2[j],"%s",palabra);
+			j++;	
+		}else{
+			j=0;
+			sprintf(buffer2[j],"%s",palabra);
+		}				
+		if(i<tam){
+			i++;
+		}else{
+			i=0;
+		}
+		
 
-	pthread_exit(0);
+//	pthread_exit(0);
 	}
+	
 }
 
 int main(int argc, char *argv[]){
 	int *b1;
-	int tam = (int) *argv[1];
+	int tam = atoi( argv[1]);
 	int i=0;
 	
-	sem_init(&datoB1,0,0);
-	sem_init(&espacioB1,0,tam);
-
+//	sem_init(&datoB1,0,0);
+//	sem_init(&espacioB1,0,tam);
+	printf("1");
 	if((buffer1=(char**)malloc(tam*sizeof(char*)))==NULL){
 		printf("ERROR al reservar memoria\n");
 		return 1;
 	}
+	printf("2");
 	for(i=0;i<tam;i++){
 		if((buffer1[i]=(char*)malloc(11*sizeof(char)))==NULL){
 			printf("ERROR al reservar memoria\n");
 			return 1;
 		}
 	}	
-	pthread_t tid[3];
-	pthread_create(&tid[0], NULL, productor, (void*) argv);
-	pthread_create(&tid[1], NULL, consumidor1, (void*) argv);
-	pthread_join(tid[0], NULL);
-	pthread_join(tid[1],NULL);
+//	pthread_t tid[3];
+	productor((void*) argv);
+//	pthread_create(&tid[0], NULL, productor, (void*) argv);
+	consumidor1((void*) argv);
+//	pthread_create(&tid[1], NULL, consumidor1, (void*) argv);
+//	pthread_join(tid[0], NULL);
+//	pthread_join(tid[1],NULL);
 	for (i=0;i<tam;i++){
-		printf("%s", buffer1[i]);
+		printf("%s\n", buffer1[i]);
 	}
+	
 	for (i=0;i<5;i++){
 		printf("%s\n", buffer2[i]);
 	}
-	sem_destroy(&datoB1);
-	sem_destroy(&espacioB1);
+//	sem_destroy(&datoB1);
+//	sem_destroy(&espacioB1);
 }
